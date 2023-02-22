fa1e89c19c76126ed60679135f68b9320239ca2d
Adrian Salazar <113551883+asalazar23@users.noreply.github.com>
2023-02-22 20:59:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+# make sure you fork this and git repo it
+# go to the shell command screen and type
+# pip install -r requirements.txt
+#to install the necessary components
+import requests
+from bs4 import BeautifulSoup
+
+# get the html
+url = "https://www.amazon.com/Best-Sellers-Books/zgbs/books"
+headers = {
+  'user-agent':
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36'
+}
+
+page = requests.get(url, headers=headers)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+
+
+#get all books
+books = soup.find_all(id = "gridItemRoot")
+
+book = books[0]
+
+rank = book.find('span', class_= 'zg-bdg-text').text[1:]
+
+children = book.find('div', class_= 'zg-grid-general-faceout').div
+
+title = children.contents[1].text
+author = children.contents[2].text
+price = children.contents[-1].text
+
+print(title)
+print(author)
+print(price)
+print(rank)
+
+
+#get info from first book
